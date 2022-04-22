fa5110b672c60de8778278f4532482d178c2a12e
atishg4 <atishghosh1793@gmail.com>
2022-04-22 21:33:28 +0530

    checking imports

--- app.py
+++ app.py
@@ -17,34 +17,37 @@ def process():
         return render_template('result.html',recommendations=recommendations)
 
 def get_recommendations(city,preferences,num_days):
-    import pandas as pd
-    import numpy as np
-    import zipfile,nltk
-    from nltk.corpus import PlaintextCorpusReader 
-    import string
-    from nltk.tokenize import word_tokenize
-    nltk.download('punkt')
-    import re
-    from nltk.corpus import stopwords
-    import heapq as hq
-    import sys
-    import nltk
-    from nltk.stem import WordNetLemmatizer
-    from nltk.tokenize import word_tokenize
-    from nltk.corpus import stopwords
-    import pandas as pd
-    import csv
-    import os
-    import string
-    import re
-    from bs4 import BeautifulSoup
-    from ast import literal_eval
-    nltk.download('stopwords')
-    from collections import Counter
+    # import pandas as pd
+    # import numpy as np
+    # import zipfile,nltk
+    # from nltk.corpus import PlaintextCorpusReader 
+    # import string
+    # from nltk.tokenize import word_tokenize
+    # nltk.download('punkt')
+    # import re
+    # from nltk.corpus import stopwords
+    # import heapq as hq
+    # import sys
+    # import nltk
+    # from nltk.stem import WordNetLemmatizer
+    # from nltk.tokenize import word_tokenize
+    # from nltk.corpus import stopwords
+    # import pandas as pd
+    # import csv
+    # import os
+    # import string
+    # import re
+    # from bs4 import BeautifulSoup
+    # from ast import literal_eval
+    # nltk.download('stopwords')
+    # from collections import Counter
+    # import math
+    # stopwords = set(stopwords.words('english'))
+
     import math
-    stopwords = set(stopwords.words('english'))
-    print("hello")
-    return {'1':'delhi','2':'Mumbai','3':'Chennai'}
+    a = math.log(4,2)
+    
+    return {'1':a,'2':'Mumbai','3':'Chennai'}
 
 
 if __name__=='__main__':
