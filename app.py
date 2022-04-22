ca01930f151ed87daaad6083bab9911ebb26b2f6
atishg4 <atishghosh1793@gmail.com>
2022-04-22 21:46:10 +0530

    ppp

--- app.py
+++ app.py
@@ -17,37 +17,50 @@ def process():
         return render_template('result.html',recommendations=recommendations)
 
 def get_recommendations(city,preferences,num_days):
-    # import pandas as pd
-    # import numpy as np
-    # import zipfile,nltk
-    # from nltk.corpus import PlaintextCorpusReader 
-    # import string
-    # from nltk.tokenize import word_tokenize
-    # nltk.download('punkt')
-    # import re
-    # from nltk.corpus import stopwords
-    # import heapq as hq
-    # import sys
-    # import nltk
-    # from nltk.stem import WordNetLemmatizer
-    # from nltk.tokenize import word_tokenize
-    # from nltk.corpus import stopwords
-    # import pandas as pd
-    # import csv
-    # import os
-    # import string
-    # import re
-    # from bs4 import BeautifulSoup
-    # from ast import literal_eval
-    # nltk.download('stopwords')
-    # from collections import Counter
-    # import math
-    # stopwords = set(stopwords.words('english'))
-
+    import pandas as pd
+    import numpy as np
+    import zipfile,nltk
+    from nltk.corpus import PlaintextCorpusReader 
+    import string
+    from nltk.tokenize import word_tokenize
+    nltk.download('punkt')
+    import re
+    from nltk.corpus import stopwords
+    import heapq as hq
+    import sys
+    import nltk
+    from nltk.stem import WordNetLemmatizer
+    from nltk.tokenize import word_tokenize
+    from nltk.corpus import stopwords
+    import pandas as pd
+    import csv
+    import os
+    import string
+    import re
+    from bs4 import BeautifulSoup
+    from ast import literal_eval
+    nltk.download('stopwords')
+    from collections import Counter
     import math
-    a = math.log(4,2)
-    
-    return {'1':a,'2':'Mumbai','3':'Chennai'}
+    stopwords = set(stopwords.words('english'))
+    stopwords.add("places")
+    stopwords.add("place")
+    df=pd.read_csv('Project_data_mid_final.csv')
+
+
+    df['Raw_Review']= df['Raw_Review'].apply(literal_eval)
+    df['new_lat_long']=df['new_lat_long'].apply(literal_eval)
+    lat=[]
+    longi=[]
+    for i in range(df.shape[0]):
+        a=df.iloc[i]['new_lat_long']
+        lat.append(a[0])
+        longi.append(a[1])
+
+    df['Latitude']=lat
+    df['Longitude']=longi
+    ar=df.iloc[0]['Latitude']
+    return {'1':ar,'2':'Mumbai','3':'Chennai'}
 
 
 if __name__=='__main__':
